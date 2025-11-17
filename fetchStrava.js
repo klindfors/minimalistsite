f8b441f766b991a67011d45dd89ff6e0a786a9c5
kimotheus <k.lindfors92@gmail.com>
2025-11-17 17:54:19 +1100

    Update fetchStrava.js

--- fetchStrava.js
+++ fetchStrava.js
@@ -2,12 +2,11 @@ import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
 
-const token = process.env.STRAVA_TOKEN; // fetch super sectret
+const token = process.env.STRAVA_TOKEN; //keys stored as github secrets
 if (!token) {
   console.error("Error: STRAVA_TOKEN is not defined.");
   process.exit(1);
 }
-
 const outputDir = "running";
 const outputFile = path.join(outputDir, "runs.json");
 const url = "https://www.strava.com/api/v3/athlete/activities?per_page=30";
@@ -22,25 +21,17 @@ async function fetchStrava() {
       console.error("Strava API error:", res.status, res.statusText);
       process.exit(1);
     }
-
     const data = await res.json();
-
     fs.mkdirSync(outputDir, { recursive: true });
-
- 
     const last30 = data.slice(0, 30);
-
- 
     let existing = [];
     if (fs.existsSync(outputFile)) {
       existing = JSON.parse(fs.readFileSync(outputFile, "utf-8"));
     }
-
     if (JSON.stringify(existing) === JSON.stringify(last30)) {
       console.log("No changes in Strava data. Skipping update.");
       process.exit(0); 
     }
-
     fs.writeFileSync(outputFile, JSON.stringify(last30, null, 2));
     console.log(`runs.json updated with ${last30.length} activities`);
   } catch (err) {
@@ -48,5 +39,4 @@ async function fetchStrava() {
     process.exit(1);
   }
 }
-
 fetchStrava();
